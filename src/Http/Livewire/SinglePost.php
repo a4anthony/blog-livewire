f7a8f4ac8f2df7fa8d4d68b4410c58486119af4c
a4anthony <55565463+a4anthony@users.noreply.github.com>
2023-05-11 18:28:14 +0100

    Update SinglePost.php

--- src/Http/Livewire/SinglePost.php
+++ src/Http/Livewire/SinglePost.php
@@ -10,24 +10,29 @@ use Livewire\Component;
 
 class SinglePost extends Component
 {
+    public BlogPost $post;
+
+    public function mount($post)
+    {
+        $this->post = $post;
+    }
     public function getPostProperty()
     {
-        return BlogPost::where('slug', request('postSlug'))->firstOrFail();
+        return BlogPost::where("slug", request("postSlug"))->firstOrFail();
     }
 
     public function getRelatedPostProperty()
     {
-        return BlogPost::where('category_id', $this->post->category_id)
-            ->where('id', '!=', $this->post->id)
+        return BlogPost::where("category_id", $this->post->category_id)
+            ->where("id", "!=", $this->post->id)
             ->limit(3)
             ->get();
     }
 
     public function render(): View|Factory|Application
     {
-        return view('blog-livewire::livewire.single-post', [
-            'post' => $this->post,
-            'relatedPosts' => $this->relatedPost,
+        return view("blog-livewire::livewire.single-post", [
+            "relatedPosts" => $this->relatedPost,
         ]);
     }
 }
